ecc417bd7f77e53f89506eadea270652bc652acb
Angela <avenable11@ivytech.edu>
2023-02-28 12:52:56 -0500

    candyland up to players on new board 2/28/2023

--- 02_28_23/candyLand.cpp
+++ 02_28_23/candyLand.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <climits>
 
 const int NUM_COLORS = 6;
 const int DECK_SIZE = 60;
@@ -23,14 +25,14 @@ const colorType colors[] = {RED,
                             GREEN,
                             BLUE,
                             ORANGE};
-const string colorStr[] = {"\033[38;5;196m\033[48;5;88m",
-                           "\033[38;5;135m\033[48;5;53m",
-                           "\033[38;5;226m\033[48;5;178m",
-                           "\033[38;5;118m\033[48;5;2m",
-                           "\033[38;5;81m\033[48;5;4m",
-                           "\033[38;5;214m\033[48;5;166m"};
-const string STARTCOLOR = "\033[1m";
-const string RESETCOLOR = "\033[0m";
+const std::string colorStr[] = {"\033[38;5;196m\033[48;5;88m",
+                                "\033[38;5;135m\033[48;5;53m",
+                                "\033[38;5;226m\033[48;5;178m",
+                                "\033[38;5;118m\033[48;5;2m",
+                                "\033[38;5;81m\033[48;5;4m",
+                                "\033[38;5;214m\033[48;5;166m"};
+const std::string STARTCOLOR = "\033[1m";
+const std::string RESETCOLOR = "\033[0m";
 
 struct cardType
 {
@@ -44,22 +46,170 @@ struct playerType
     colorType color;
     int position = 0;
 };
-
-void generateBoard(colorType[], int);
-std::string getColorName(colorType); // complete for lecture activity
-
 struct playerListType
 {
     playerType playerList[MAX_PLAYERS];
     int numPlayers;
 };
 
+void generateBoard(colorType[], int);
+std::string getColorName(colorType); // complete for lecture activity
+void printBoard(colorType[], int, playerListType &);
+std::string printConsoleColor(colorType);
+void setupPlayers(playerListType &);
+int getNumPlayers();
+playerType createAPlayer(int);
+void resetStream();
+colorType inputColor();
+void printPlayerOnBoard(playerListType &, int, std::string);
 int main()
 {
     colorType board[BOARD_SIZE];
     generateBoard(board, BOARD_SIZE);
+    playerListType playerList;
+    setupPlayers(playerList);
+    printBoard(board, BOARD_SIZE, playerList);
 }
 
-void generateBoard(colorType[], int)
+void generateBoard(colorType board[], int spaces)
 {
+    std::ifstream in;
+    in.open("boardSequence.txt");
+    board[0] = START;
+    for (int i = 1; i < spaces; i++)
+    {
+        int c;
+        in >> c;
+        board[i] = colors[c % NUM_COLORS];
+    }
+    in.close();
+}
+
+void printBoard(colorType board[], int spaces, playerListType &pl)
+{
+    for (int i = 0; i < spaces; i++)
+    {
+        std::cout << printConsoleColor(board[i]) << "[ ";
+        printPlayerOnBoard(pl, i, printConsoleColor(board[i]));
+        std::cout << " ]";
+        std::cout << RESETCOLOR << " ";
+        if ((i + 1) % 5 == 0)
+        {
+            std::cout << std::endl
+                      << std::endl;
+        }
+    }
+}
+
+std::string printConsoleColor(colorType c)
+{
+    std::string out = STARTCOLOR;
+    if (c == START)
+    {
+        out = RESETCOLOR;
+    }
+    else
+    {
+        out += colorStr[c];
+    }
+    return out;
+}
+
+void setupPlayers(playerListType &pl)
+{
+    pl.numPlayers = getNumPlayers();
+    for (int i = 0; i < pl.numPlayers; i++)
+    {
+        pl.playerList[i] = createAPlayer(i + 1);
+        std::cout << "Hello " << pl.playerList[i].name << "! You can now pick a color." << std::endl;
+        pl.playerList[i].color = inputColor();
+        for (int j = 0; j < i; j++)
+        {
+            if (pl.playerList[j].color == pl.playerList[i].color)
+            {
+                std::cout << pl.playerList[j].name << " has alread picked that color. Please pick again." << std::endl;
+                pl.playerList[i].color = inputColor();
+                j = -1;
+            }
+        }
+    }
+}
+
+int getNumPlayers()
+{
+    int numPlayers;
+    std::cout << "Enter the number of players between 2 and " << MAX_PLAYERS << ": ";
+    std::cin >> numPlayers;
+    std::cout << std::endl;
+    while (!std::cin || numPlayers < 2 || numPlayers > MAX_PLAYERS)
+    {
+        if (!std::cin)
+        {
+            resetStream();
+        }
+        std::cout << "That is not a valid value." << std::endl;
+        std::cout << "Enter the number of players between 2 and " << MAX_PLAYERS << ": ";
+        std::cin >> numPlayers;
+        std::cout << std::endl;
+    }
+    return numPlayers;
+}
+
+playerType createAPlayer(int n)
+{
+    playerType newPlayer;
+    std::cout << "Player " << n << " enter yoru name: ";
+    std::getline(std::cin >> std::ws, newPlayer.name);
+    return newPlayer;
+}
+
+colorType inputColor()
+{
+    int num;
+    colorType myColor;
+    std::cout << "Choose a color:" << std::endl;
+    for (int i = 0; i < NUM_COLORS; i++)
+    {
+        std::cout << i + 1 << ". " << colorName[i] << std::endl;
+    }
+    std::cin >> num;
+    std::cout << std::endl;
+    while (!std::cin || num - 1 < RED || num - 1 > ORANGE)
+    {
+        if (!std::cin)
+        {
+            resetStream();
+        }
+        std::cout << "Please choose a color using the number next to the color name." << std::endl;
+        std::cin >> num;
+        std::cout << std::endl;
+    }
+    myColor = colors[num - 1];
+
+    return myColor;
+}
+
+void printPlayerOnBoard(playerListType &pl, int boardSpace, std::string spaceColor)
+{
+    for (int i = 0; i < pl.numPlayers; i++)
+    {
+        if (pl.playerList[i].position == boardSpace)
+        {
+            std::cout << printConsoleColor(pl.playerList[i].color);
+            std::cout << i + 1;
+            std::cout << spaceColor;
+        }
+        else
+        {
+            std::cout << " ";
+        }
+    }
+}
+
+void resetStream()
+{
+
+    std::cin.clear();
+    std::cin.ignore(INT_MAX, '\n');
+    std::cout << "You entered something that is not a number." << std::endl;
 }
